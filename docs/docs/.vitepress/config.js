caaa0d3c06aeb71e7e11e964e93cb68fc44b44f2
jiangshuaijie <1830702951@qq.com>
2022-11-14 18:56:38 +0800

    初始化

---
+++ docs/docs/.vitepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    title: '设计系统',
+    description: '设计系统的相关资料',
+    base: '/DesignSystem/',
+    themeConfig: {
+        // siteTitle: false,
+        // logo: "/logo.svg",
+        nav: [
+            // {
+            //     text: '',
+            //     link: "/"
+            // }
+        ],
+        outlineTitle: '在本页面',
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2022-present codeteenager'
+        },
+        socialLinks: [{ icon: "github", link: "https://github.com/codeteenager/DesignSystem" }],
+        // sidebar: {
+        //     "/web/": [
+
+        //         {
+        //             text: "基础",
+        //             items: [
+        //                 {
+        //                     text: "介绍",
+        //                     link: "/web/introduction",
+        //                 }
+        //             ],
+        //         }
+        //     ],
+        // },
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        }
+    }
+}
---
+++ docs/docs/index.md
@@ -0,0 +1,24 @@
+# 设计系统的相关资料
+
+## sketch插件开发
+* [深入浅出 Sketch 插件开发](https://cloud.tencent.com/developer/article/1991121)
+* [携程机票Sketch插件开发实践](https://zhuanlan.zhihu.com/p/71015659)
+* [Sketch 插件开发实践总结](https://jelly.jd.com/article/5dbbe282b73b47015299a001)
+* [积木Sketch插件进阶开发指南](https://zhuanlan.zhihu.com/p/275884688)
+* [积木Sketch Plugin：设计同学的贴心搭档](https://tech.meituan.com/2020/05/21/waimai-sketch-plugin.html)
+* [Sketch插件开发总结](https://zhuanlan.zhihu.com/p/28325423)
+
+## UI2Code
+* [58的picasso](https://github.com/wuba/Picasso)
+* [kitchen](https://kitchen.alipay.com/)
+* [CodeFun](https://code.fun/)
+* [CoDesign](https://codesign.qq.com/)
+
+## 技术分享
+
+* [设计工程化三部曲](/DesignSystem/pdf/设计工程化三部曲.pdf)
+* [探索中后台设计提效次时代](/DesignSystem/pdf/探索中后台设计提效次时代.pdf)
+* [提效神奇DesignToken的探索与应用.pdf](/DesignSystem/pdf/提效神奇DesignToken的探索与应用.pdf)
+* [AntDesign资产工程化探索和实践](/DesignSystem/pdf/AntDesign资产工程化探索和实践.pdf)
+* [FusionDesign-解构设计，抽象工程](/lowcode/pdf/FusionDesign-解构设计，抽象工程.pdf)
+
